c5da9fd8a3823df37daaac388647688c7d3cf8c4
Michael L Perry <michael@qedcode.com>
2014-11-24 22:56:55 -0600

    Use prototypes on Correspondence facts.

--- Correspondence.js
+++ Correspondence.js
@@ -8,10 +8,10 @@ function CardColumn(card, column, prior) {
 
 function Card(project, created) {
     this._textAdapters = [];
+};
 
-    this.text = function (textAdapter) {
-        this._textAdapters.push(textAdapter);
-    };
+Card.prototype.text = function (textAdapter) {
+    this._textAdapters.push(textAdapter);
 };
 
 function ColumnName(column, value, prior) {
@@ -24,10 +24,10 @@ function ColumnOrdinal(column, value, prior) {
 
 function Column(project) {
     this._cardAdapters = [];
+};
 
-    this.cards = function (cardAdapter) {
-        this._cardAdapters.push(cardAdapter);
-    };
+Column.prototype.cards = function (cardAdapter) {
+    this._cardAdapters.push(cardAdapter);
 };
 
 function Member(individual, project) {
@@ -41,90 +41,91 @@ function ProjectName(project, value, prior) {
 function Project(created) {
     this._nameAdapters = [];
     this._columnAdapters = [];
+};
 
-    this.name = function (nameAdapter) {
-        this._nameAdapters.push(nameAdapter);
-    };
+Project.prototype.name = function (nameAdapter) {
+    this._nameAdapters.push(nameAdapter);
+};
 
-    this.columns = function (columnAdapter) {
-        this._columnAdapters.push(columnAdapter);
-    };
+Project.prototype.columns = function (columnAdapter) {
+    this._columnAdapters.push(columnAdapter);
 };
 
 function Individual() {
     this._projectAdapters = [];
+};
 
-    this.projects = function (projectAdapter) {
-        this._projectAdapters.push(projectAdapter);
-    };
+Individual.prototype.projects = function (projectAdapter) {
+    this._projectAdapters.push(projectAdapter);
 };
 
 function Community(url) {
-    this.load = function (memento) {
-        var individual = this.newIndividual();
-        return individual;
-    };
+};
 
-    this.newIndividual = function () {
-        return new Individual();
-    };
+Community.prototype.load = function (memento) {
+    var individual = this.newIndividual();
+    return individual;
+};
 
-    this.newProject = function (created) {
-        return new Project(created);
-    };
+Community.prototype.newIndividual = function () {
+    return new Individual();
+};
 
-    this.newProjectName = function (project, value, prior) {
-        var projectName = new ProjectName(project, value, prior);
-        for (index in project._nameAdapters) {
-            var nameAdapter = project._nameAdapters[index];
-            nameAdapter.added(projectName);
-        }
-        return projectName;
-    };
+Community.prototype.newProject = function (created) {
+    return new Project(created);
+};
 
-    this.newMember = function (individual, project) {
-        for (index in individual._projectAdapters) {
-            var projectAdapter = individual._projectAdapters[index];
-            projectAdapter.added(project);
-        }
-        return new Member(individual, project);
-    };
+Community.prototype.newProjectName = function (project, value, prior) {
+    var projectName = new ProjectName(project, value, prior);
+    for (index in project._nameAdapters) {
+        var nameAdapter = project._nameAdapters[index];
+        nameAdapter.added(projectName);
+    }
+    return projectName;
+};
 
-    this.newColumn = function (project) {
-        var column = new Column(project);
-        for (index in project._columnAdapters) {
-            var columnAdapter = project._columnAdapters[index];
-            columnAdapter.added(column);
-        }
-        return column;
-    };
+Community.prototype.newMember = function (individual, project) {
+    for (index in individual._projectAdapters) {
+        var projectAdapter = individual._projectAdapters[index];
+        projectAdapter.added(project);
+    }
+    return new Member(individual, project);
+};
 
-    this.newColumnName = function (column, value, prior) {
-        return new ColumnName(column, value, prior);
-    };
+Community.prototype.newColumn = function (project) {
+    var column = new Column(project);
+    for (index in project._columnAdapters) {
+        var columnAdapter = project._columnAdapters[index];
+        columnAdapter.added(column);
+    }
+    return column;
+};
 
-    this.newColumnOrdinal = function (column, value, prior) {
-        return new ColumnOrdinal(column, value, prior);
-    };
+Community.prototype.newColumnName = function (column, value, prior) {
+    return new ColumnName(column, value, prior);
+};
 
-    this.newCard = function (project, created) {
-        return new Card();
-    };
+Community.prototype.newColumnOrdinal = function (column, value, prior) {
+    return new ColumnOrdinal(column, value, prior);
+};
 
-    this.newCardColumn = function (card, column, prior) {
-        for (index in column._cardAdapters) {
-            var cardAdapter = column._cardAdapters[index];
-            cardAdapter.added(card);
-        }
-        return new CardColumn(card, column, prior);
-    };
+Community.prototype.newCard = function (project, created) {
+    return new Card();
+};
 
-    this.newCardText = function (card, value, prior) {
-        var cardText = new CardText(card, value, prior);
-        for (index in card._textAdapters) {
-            var textAdapter = card._textAdapters[index];
-            textAdapter.added(cardText);
-        }
-        return cardText;
-    };
-}
+Community.prototype.newCardColumn = function (card, column, prior) {
+    for (index in column._cardAdapters) {
+        var cardAdapter = column._cardAdapters[index];
+        cardAdapter.added(card);
+    }
+    return new CardColumn(card, column, prior);
+};
+
+Community.prototype.newCardText = function (card, value, prior) {
+    var cardText = new CardText(card, value, prior);
+    for (index in card._textAdapters) {
+        var textAdapter = card._textAdapters[index];
+        textAdapter.added(cardText);
+    }
+    return cardText;
+};
